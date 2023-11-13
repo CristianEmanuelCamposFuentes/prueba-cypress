f6b2568497f27fb68bc564035ee456e3a316129c
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-12 23:49:55 -0300

    Seteando acordeon

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,8 +1,11 @@
 describe('Fundamentals test', () => {
-  it('passes', () => {
+  it('Contains correct header text', () => {
     cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+//    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
     cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing')
-
+  })
+  it('Accordion works correctly', () => {
+    cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test="accordion-item-1"]').click().pause()
   })
 })
